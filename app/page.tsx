9494a75be55f31d8fdced1b76ed045e01bb3ff7e
Rithik2211 <rithikpranao22@gmail.com>
2025-01-19 19:41:30 +0530

    initial commit for the application

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-white flex justify-center items-center h-screen w-full">
+      <h1>Hello Trust hash</h1>
+    </div>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row h-[45px] w-full bg-transparent'>
+        
+    </div>
+  )
+}
+
+export default Navbar;
