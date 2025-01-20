9c42dbeadf28b466f42e7367d00b956e7b0b6fd8
Rithik2211 <rithikpranao22@gmail.com>
2025-01-21 00:25:18 +0530

    updated nav image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Button } from './ui/button';
+import Image from 'next/image';
 
 const Navbar = () => {
     const navPages = ['Home', 'Features', 'Services', 'Products']
   return (
     <div className='flex flex-row h-[70px] w-full backdrop-blur justify-around items-center text-white fixed top-0 left-0 z-10'>
-        <div className=''>
+        <div className='flex flex-row justify-center items-center text-center'>
+            <Image src={'/trust-hash.png'} width={70} height={70} alt='logo' />
             <h1 className='text-[25px] font-bold'>Trust Hash</h1>
         </div>
         <div className='flex flex-row font-semibold'>
