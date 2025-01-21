117343285eb06ba2654ad5376226356526134b9a
Rohith1510 <rohithpranav567@gmail.com>
2025-01-22 00:20:05 +0530

    Feat:Added two components

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import About from "./pages/about/page";
 import Footer from "./pages/footer/page";
 import HeaderTitle from "./pages/header/page";
 import Product from "./pages/product/page";
+import Solution from "./pages/Solutions/page";
+import Work from "./pages/work/page";
 
 export default function Home() {
   return (
     <div className="text-white flex flex-col justify-center items-center h-full w-full">
       <HeaderTitle />
       <Product />
+      <Solution />
+      <Work />
       <About />
       <Footer />
     </div>
