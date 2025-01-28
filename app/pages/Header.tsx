39ffc7873ec37cefed1cb7a44e800560eb38be16
Rohith Pranav Bala <132284060+Rohith1510@users.noreply.github.com>
2025-01-28 22:54:13 +0530

    Refactor:Content changes (#5)

--- app/pages/Header.tsx
+++ app/pages/Header.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image';
 import React from 'react'
 
 const HeaderTitle = ({ id }: { id?: string }) => {
-    const description = 'Secure, transparent and efficient solutions for the digital age. Harness the power of blockchain technology to transform your operations.'
+    const description = 'Seamless blockchain solutions to modernize, innovate, and grow. Empowering businesses for a smarter, sustainable future.'
   return (
     <div id={id} className='flex flex-row justify-center items-center w-full h-screen text-white px-4'>
         <div className='flex flex-col text-center justify-center items-center gap-4 w-full h-screen'>
-            <h1 className='text-[40px] md:text-[55px] font-bold px-4'><span className='text-green-500' >Revolutionize </span> Your <span className='text-green-500'>Business</span> with <span className=' text-green-500'>Blockchain</span> </h1>
+            <h1 className='text-[40px] md:text-[55px] font-bold px-4'><span className='text-green-500' >Revolutionize </span> Your <span className='text-green-500'>Business</span> with Trust <span className=' text-green-500'>Hash</span> Solutions </h1>
             <p className='text-[22px] font-medium px-4 mx-2 '>{description}</p>
         </div>
         <div className='hidden md:block w-[800px]'>
--- app/pages/Solutions.tsx
+++ app/pages/Solutions.tsx
@@ -1,23 +1,23 @@
-import { CheckCircle, Layers, Zap } from "lucide-react"
+import { Code,Lightbulb,TrendingUp } from "lucide-react"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 const solutions = [
     {
-      icon: <Zap className="h-8 w-8 text-green-600" />,
-      title: "Seamless Adoption",
+      icon: <Code className="h-8 w-8 text-green-600" />,
+      title: "Custom Blockchain Development",
       description:
-        "We make blockchain adoption seamless for businesses, modernizing operations and keeping them ahead of the curve.",
+        "Create bespoke blockchain solutions like NFTs, smart contracts, and decentralized apps tailored to your unique business needs.",
     },
     {
-      icon: <Layers className="h-8 w-8 text-green-600" />,
-      title: "Tailored Solutions",
+      icon: <Lightbulb className="h-8 w-8 text-green-600" />,
+      title: "  Blockchain Integration & Consulting",
       description:
-        "We unlock new revenue opportunities by designing blockchain solutions tailored to your specific business needs.",
+        "Simplify blockchain adoption with seamless system integration and expert consultations, empowering your business to innovate and grow.",
     },
     {
-      icon: <CheckCircle className="h-8 w-8 text-green-600" />,
-      title: "Education & Support",
+      icon: <TrendingUp className="h-8 w-8 text-green-600" />,
+      title: "Revenue Growth with Blockchain",
       description:
-        "We educate businesses through workshops, resources, and consultations, empowering them with essential blockchain knowledge.",
+        "Unlock new revenue streams by implementing innovative blockchain-based business models designed for scalability and success.",
     },
   ]
 
